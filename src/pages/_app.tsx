26c92cf7faca9b3bf8808f001c38d41d8be23bcc
Blake Frederick <bf@six12creative.com>
2022-11-27 23:20:21 -0800

    🏖️

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app"
+import { type Session } from "next-auth"
+import { SessionProvider } from "next-auth/react"
+import { ThirdwebProvider } from "@thirdweb-dev/react/solana"
+import { Network } from "@thirdweb-dev/sdk/solana"
+import { WalletProvider } from "@solana/wallet-adapter-react"
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
 
 import "../styles/globals.css";
+
+const network: Network = 'devnet'
+const domain = 'educationalpurposes.edu'
+const wallet = new PhantomWalletAdapter()ß
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
